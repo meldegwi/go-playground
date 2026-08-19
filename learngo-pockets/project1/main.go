1e3d93c799447ba4650ecd95c344cd8a255f07bb
Mohammed ElDegwi <mohammedeldegwi@gmail.com>
2026-08-19 14:53:17 +0300

    feat: expand language support
    Signed-off-by: Mohammed ElDegwi <mohammedeldegwi@gmail.com>

--- learngo-pockets/project1/main.go
+++ learngo-pockets/project1/main.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	greeting := greet("en")
+	fmt.Println(greeting)
+}
+
+type language string
+
+func greet(l language) string {
+	switch l {
+	case "en":
+		return "Hello, World!"
+	case "fr":
+		return "Bonjour le monde!"
+	default:
+		return "Unsupported language :( Please try again"
+	}
 }
--- learngo-pockets/project1/main_internal_test.go
+++ learngo-pockets/project1/main_internal_test.go
@@ -1,7 +1,19 @@
 package main
 
+import "testing"
+
 func Example_main() {
 	main()
 	// Output:
 	// Hello, World!
+}
+
+func TestGreet(t *testing.T) {
+	want := "Hello, World!"
+
+	got := greet()
+
+	if got != want {
+		t.Errorf("expected: %q, got: %q", want, got)
+	}
 }

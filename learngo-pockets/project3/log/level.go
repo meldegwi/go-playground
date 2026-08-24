e8eacb53ef340bea5528162e3f27d7435f33efcb
Mohammed ElDegwi <mohammedeldegwi@gmail.com>
2026-08-24 12:58:42 +0300

    feat: initate the project
    Signed-off-by: Mohammed ElDegwi <mohammedeldegwi@gmail.com>

---
+++ learngo-pockets/project3/log/level.go
@@ -0,0 +1,4 @@
+package log
+
+// Level represents an available logging level
+type Level byte
---
+++ learngo-pockets/project3/log/logger.go
@@ -0,0 +1,5 @@
+// Package log...
+package log
+
+// Logger is used to log information.
+type Logger struct{}

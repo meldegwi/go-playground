458aabff02856c3ddb6dcf3ed7fedb511c482570
Mohammed ElDegwi <mohammedeldegwi@gmail.com>
2026-08-24 20:05:32 +0300

    feat: declare severity levels
    Signed-off-by: Mohammed ElDegwi <mohammedeldegwi@gmail.com>

--- learngo-pockets/project3/log/level.go
+++ learngo-pockets/project3/log/level.go
@@ -2,3 +2,21 @@ package log
 
 // Level represents an available logging level
 type Level byte
+
+const (
+	// LevelDebug represents the lowest level of log, mostly used
+	// for debugging purposes.
+	LevelDebug Level = iota
+	// LevelInfo represents a logging level that contains information
+	// deemed valuable.
+	LevelInfo
+	// LevelWarn represents a cautious notes about events that
+	// unexpected/undesireable but the system recovered from them.
+	LevelWarn
+	// LevelError represents a high logging level, only to be
+	// used to trace errors.
+	LevelError
+	// LevelFatal represents the highest logging level, only to be
+	// used in major error that are unrecoverable.
+	LevelFatal
+)

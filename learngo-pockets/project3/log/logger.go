18ea9b40258d5182539802e51ce76ae1ca3063e2
Mohammed ElDegwi <mohammedeldegwi@gmail.com>
2026-08-24 20:51:14 +0300

    feat: define log API
    Signed-off-by: Mohammed ElDegwi <mohammedeldegwi@gmail.com>

--- learngo-pockets/project3/log/logger.go
+++ learngo-pockets/project3/log/logger.go
@@ -2,4 +2,28 @@
 package log
 
 // Logger is used to log information.
-type Logger struct{}
+type Logger struct {
+	threshold Level
+}
+
+// New returns news you a logger, ready to log at the required threshold.
+func New(threshold Level) *Logger {
+	return &Logger{
+		threshold: threshold,
+	}
+}
+
+// Debugf formats and prints a message if a log level is debug.
+func (l *Logger) Debugf(format string, args ...any) {
+	// implement me.
+}
+
+// Infof formats and prints a message if a log level is info.
+func (l *Logger) Infof(format string, args ...any) {
+	// implement me.
+}
+
+// Errorf formats and prints a message if a log level is error.
+func (l *Logger) Errorf(format string, args ...any) {
+	// implement me.
+}

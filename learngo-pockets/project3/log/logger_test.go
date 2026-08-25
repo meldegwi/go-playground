192e1642b0688eb982bb85dc2a0c163e1c67f407
Mohammed ElDegwi <mohammedeldegwi@gmail.com>
2026-08-25 14:29:28 +0300

    docs: fix typos
    Signed-off-by: Mohammed ElDegwi <mohammedeldegwi@gmail.com>

--- learngo-pockets/project3/log/logger_test.go
+++ learngo-pockets/project3/log/logger_test.go
@@ -1,5 +1,5 @@
 package log_test
 
-// testing will be skiped this time due to added time stamps to logs.
+// testing will be skipped this time due to added time stamps to logs.
 // which require more skills. it will be added in the future.
 // TODO: write lib tests.

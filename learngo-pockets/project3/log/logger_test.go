88e1388edc2bdecaea530fe90cc78e9be3617feb
Mohammed ElDegwi <mohammedeldegwi@gmail.com>
2026-08-25 12:14:02 +0300

    feat: write the main function
    Signed-off-by: Mohammed ElDegwi <mohammedeldegwi@gmail.com>

--- learngo-pockets/project3/log/logger_test.go
+++ learngo-pockets/project3/log/logger_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func ExampleLogger_Debugf() {
-	dl := log.New(log.LevelDebug, os.Stdout)
+	dl := log.New(log.LevelDebug, log.WithOutput(os.Stdout))
 	dl.Debugf("Hello, %s", "world!")
 	// Output: [DEBUG] Hello, world!
 }

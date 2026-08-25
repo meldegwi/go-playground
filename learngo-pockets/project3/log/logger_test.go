7683eab1d850ab73913cb998d308c8994d8634b7
Mohammed ElDegwi <mohammedeldegwi@gmail.com>
2026-08-25 13:17:55 +0300

    refactor: remove the naive test
    Signed-off-by: Mohammed ElDegwi <mohammedeldegwi@gmail.com>

--- learngo-pockets/project3/log/logger_test.go
+++ learngo-pockets/project3/log/logger_test.go
@@ -1,13 +1,1 @@
 package log_test
-
-import (
-	"os"
-
-	"logger/log"
-)
-
-func ExampleLogger_Debugf() {
-	dl := log.New(log.LevelDebug, log.WithOutput(os.Stdout))
-	dl.Debugf("Hello, %s", "world!")
-	// Output: [DEBUG] Hello, world!
-}

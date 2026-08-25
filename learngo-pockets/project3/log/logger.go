8e2151c20ac239112b62872549b41c46fc0a6768
Mohammed ElDegwi <mohammedeldegwi@gmail.com>
2026-08-25 09:46:16 +0300

    refactor: add io.Writer to write to desired dest
    Signed-off-by: Mohammed ElDegwi <mohammedeldegwi@gmail.com>

--- learngo-pockets/project3/log/logger.go
+++ learngo-pockets/project3/log/logger.go
@@ -1,70 +1,77 @@
 package log
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 // Logger is used to log information.
 type Logger struct {
 	threshold Level
+	output    io.Writer
 }
 
 // New returns news you a logger, ready to log at the required threshold.
-func New(threshold Level) *Logger {
+// the default output is Stdout
+func New(threshold Level, output io.Writer) *Logger {
 	return &Logger{
 		threshold: threshold,
+		output:    output,
 	}
 }
 
 // Debugf formats and prints a message if a log level is debug or beyond.
 func (l *Logger) Debugf(format string, args ...any) {
-	if l.threshold > LevelDebug {
-		return
+	if l.output == nil {
+		l.output = os.Stdout
 	}
 
-	message := fmt.Sprintf(format, args...)
-
-	fmt.Printf("[DEBUG] %s", message)
+	if l.threshold <= LevelDebug {
+		_, _ = fmt.Fprintf(l.output, "[DEBUG] "+format, args...)
+	}
 }
 
 // Infof formats and prints a message if a log level is info or beyond.
 func (l *Logger) Infof(format string, args ...any) {
-	if l.threshold > LevelInfo {
-		return
+	if l.output == nil {
+		l.output = os.Stdout
 	}
 
-	message := fmt.Sprintf(format, args...)
-
-	fmt.Printf("[INFO] %s", message)
+	if l.threshold <= LevelInfo {
+		_, _ = fmt.Fprintf(l.output, "[INFO] "+format, args...)
+	}
 }
 
 // Warnf formats and prints a message if a log level is warn or beyond.
 func (l *Logger) Warnf(format string, args ...any) {
-	if l.threshold > LevelWarn {
-		return
+	if l.output == nil {
+		l.output = os.Stdout
 	}
 
-	message := fmt.Sprintf(format, args...)
-
-	fmt.Printf("[WARN] %s", message)
+	if l.threshold <= LevelWarn {
+		_, _ = fmt.Fprintf(l.output, "[WARN] "+format, args...)
+	}
 }
 
 // Errorf formats and prints a message if a log level is error or beyond.
 func (l *Logger) Errorf(format string, args ...any) {
-	if l.threshold > LevelError {
-		return
+	if l.output == nil {
+		l.output = os.Stderr
 	}
 
-	message := fmt.Sprintf(format, args...)
-
-	fmt.Printf("[ERROR] %s", message)
+	if l.threshold <= LevelError {
+		_, _ = fmt.Fprintf(l.output, "[ERROR] "+format, args...)
+	}
 }
 
 // Fatalf formats and prints a message if a log level is fatal.
 func (l *Logger) Fatalf(format string, args ...any) {
-	if l.threshold > LevelFatal {
-		return
+	if l.output == nil {
+		l.output = os.Stderr
 	}
 
-	message := fmt.Sprintf(format, args...)
-
-	fmt.Printf("[FATAL] %s", message)
+	if l.threshold <= LevelFatal {
+		_, _ = fmt.Fprintf(l.output, "[FATAL] "+format, args...)
+	}
 }
--- learngo-pockets/project3/log/logger_test.go
+++ learngo-pockets/project3/log/logger_test.go
@@ -1,9 +1,13 @@
 package log_test
 
-import "logger/log"
+import (
+	"os"
+
+	"logger/log"
+)
 
 func ExampleLogger_Debugf() {
-	dl := log.New(log.LevelDebug)
+	dl := log.New(log.LevelDebug, os.Stdout)
 	dl.Debugf("Hello, %s", "world!")
 	// Output: [DEBUG] Hello, world!
 }

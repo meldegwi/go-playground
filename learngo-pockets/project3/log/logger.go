ef20ea0a55609697a7cd434fd3faef4f13b6165a
Mohammed ElDegwi <mohammedeldegwi@gmail.com>
2026-08-25 11:33:25 +0300

    refactor: seperate conserns an remove redundent code
    Signed-off-by: Mohammed ElDegwi <mohammedeldegwi@gmail.com>

--- learngo-pockets/project3/log/logger.go
+++ learngo-pockets/project3/log/logger.go
@@ -23,59 +23,39 @@ func New(threshold Level, output io.Writer) *Logger {
 
 // Debugf formats and prints a message if a log level is debug or beyond.
 func (l *Logger) Debugf(format string, args ...any) {
-	if l.output == nil {
-		l.output = os.Stdout
-	}
-
-	if l.threshold <= LevelDebug {
-		l.logf(format, DEBUG, args...)
-	}
+	l.logf(LevelDebug, DEBUG, format, args...)
 }
 
 // Infof formats and prints a message if a log level is info or beyond.
 func (l *Logger) Infof(format string, args ...any) {
-	if l.output == nil {
-		l.output = os.Stdout
-	}
-
-	if l.threshold <= LevelInfo {
-		l.logf(format, INFO, args...)
-	}
+	l.logf(LevelInfo, INFO, format, args...)
 }
 
 // Warnf formats and prints a message if a log level is warn or beyond.
 func (l *Logger) Warnf(format string, args ...any) {
-	if l.output == nil {
-		l.output = os.Stdout
-	}
-
-	if l.threshold <= LevelWarn {
-		l.logf(format, WARN, args...)
-	}
+	l.logf(LevelWarn, WARN, format, args...)
 }
 
 // Errorf formats and prints a message if a log level is error or beyond.
 func (l *Logger) Errorf(format string, args ...any) {
-	if l.output == nil {
-		l.output = os.Stderr
-	}
-
-	if l.threshold <= LevelError {
-		l.logf(format, ERROR, args...)
-	}
+	l.logf(LevelError, ERROR, format, args...)
 }
 
 // Fatalf formats and prints a message if a log level is fatal.
 func (l *Logger) Fatalf(format string, args ...any) {
-	if l.output == nil {
-		l.output = os.Stderr
-	}
-
-	if l.threshold <= LevelFatal {
-		l.logf(format, FATAL, args...)
-	}
+	l.logf(LevelFatal, FATAL, format, args...)
+	os.Exit(1)
 }
 
-func (l *Logger) logf(format string, level string, args ...any) {
-	_, _ = fmt.Fprintf(l.output, level+" "+format+"\n", args...)
+func (l *Logger) logf(level Level, prefix, format string, args ...any) {
+	if l.threshold > level {
+		return
+	}
+
+	if l.output == nil {
+		l.output = os.Stdout
+	}
+
+	msg := fmt.Sprintf(format, args...)
+	_, _ = fmt.Fprintf(l.output, "%s %s\n", prefix, msg)
 }

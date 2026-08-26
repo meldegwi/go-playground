e243b1bb3003bab35b1ce96c73516964382ee928
Mohammed ElDegwi <mohammedeldegwi@gmail.com>
2026-08-26 21:30:04 +0300

    feat: add corpus file path as cli flag
    Signed-off-by: Mohammed ElDegwi <mohammedeldegwi@gmail.com>

--- learngo-pockets/project4/main.go
+++ learngo-pockets/project4/main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
 
 	"gordle/gordle"
 )
 
 func main() {
-	g := gordle.New(os.Stdin, "hello", 5)
+	var filePath string
+	flag.StringVar(&filePath, "corpus-fp", "corpus/words",
+		"Define corpus file pass as needed.")
+	flag.Parse()
+
+	corpus, err := gordle.ReadCorpus(filePath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s", err.Error())
+	}
+
+	g, err := gordle.New(os.Stdin, corpus, 5)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s", err.Error())
+	}
+
 	g.Play()
 }

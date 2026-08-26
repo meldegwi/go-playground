e05fbf436a09b68345e33584e034adf5fd574760
Mohammed ElDegwi <mohammedeldegwi@gmail.com>
2026-08-26 08:43:50 +0300

    feat: implement the ask method
    Signed-off-by: Mohammed ElDegwi <mohammedeldegwi@gmail.com>

--- learngo-pockets/project4/gordle/game.go
+++ learngo-pockets/project4/gordle/game.go
@@ -4,7 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"os"
 )
+
+const solutionLength = 5
 
 // Game holds all the information we need to play a game of gordle.
 type Game struct {
@@ -23,4 +26,25 @@ func (g *Game) Play() {
 	fmt.Println("Welcome to Gordle!")
 
 	fmt.Printf("Enter a guess:\n")
+}
+
+func (g *Game) ask() []rune {
+	fmt.Printf("Enter a %d-character guess:\n", solutionLength)
+
+	for {
+		playerInput, _, err := g.reader.ReadLine()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Gordle failed to read your guess: %s\n",
+				err.Error())
+		}
+
+		guess := []rune(string(playerInput))
+		if len(guess) != solutionLength {
+			fmt.Fprintf(os.Stderr,
+				"Oh oh... Your guess is %d-character but it must be a %d-character word. Please try again.\n",
+				len(guess), solutionLength)
+		} else {
+			return guess
+		}
+	}
 }

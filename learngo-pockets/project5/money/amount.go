c38171744e7ccf40d9e1f6a803c90b98780b9c56
Mohammed ElDegwi <mohammedeldegwi@gmail.com>
2026-09-01 07:26:14 +0300

    feat: implement new ammount func
    Signed-off-by: Mohammed ElDegwi <mohammedeldegwi@gmail.com>

--- learngo-pockets/project5/money/amount.go
+++ learngo-pockets/project5/money/amount.go
@@ -1,6 +1,25 @@
 package money
 
+import "fmt"
+
+const (
+	ErrIncompatibleDeciCurr = Error("Decimal percision and Currency percision are not compatible")
+)
+
 type Amount struct {
 	quantity Decimal
 	currency Currency
+}
+
+func NewAmount(quantity Decimal, currency Currency) (Amount, error) {
+	if quantity.percision > currency.percision {
+		return Amount{}, fmt.Errorf("%w: too percise", ErrIncompatibleDeciCurr)
+	}
+
+	quantity.percision = currency.percision
+
+	return Amount{
+		quantity: quantity,
+		currency: currency,
+	}, nil
 }

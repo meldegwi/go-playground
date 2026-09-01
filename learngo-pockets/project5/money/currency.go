057d9ff55eb6d02023424244e9f4e9b7434674cb
Mohammed ElDegwi <mohammedeldegwi@gmail.com>
2026-09-01 06:28:51 +0300

    feat: implement currency parser
    Signed-off-by: Mohammed ElDegwi <mohammedeldegwi@gmail.com>

--- learngo-pockets/project5/money/currency.go
+++ learngo-pockets/project5/money/currency.go
@@ -1,6 +1,42 @@
 package money
 
+import "unicode"
+
+const (
+	ErrInvalidCurrencyCode = Error("invalid currency code")
+)
+
 // Currency defines code of the currency.
 type Currency struct {
-	code string
+	code      string
+	percision byte
+}
+
+func Parse(code string) (Currency, error) {
+	if len(code) != 3 {
+		return Currency{}, ErrInvalidCurrencyCode
+	}
+
+	for _, r := range code {
+		if !unicode.IsLetter(r) && !unicode.IsUpper(r) {
+			return Currency{}, ErrInvalidCurrencyCode
+		}
+	}
+
+	var perc byte
+	switch code {
+	case "IRR":
+		perc = 0
+	case "CNY", "VND":
+		perc = 1
+	case "BHD", "IQD", "KWD", "LYD", "OMR", "TND":
+		perc = 3
+	default:
+		perc = 2
+	}
+
+	return Currency{
+		code:      code,
+		percision: perc,
+	}, nil
 }

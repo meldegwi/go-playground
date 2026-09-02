b191f44739ed442ecff59f6d97ba0550f0bef7af
Mohammed ElDegwi <mohammedeldegwi@gmail.com>
2026-09-02 19:14:19 +0300

    feat: implement converter cli
    Signed-off-by: Mohammed ElDegwi <mohammedeldegwi@gmail.com>

--- learngo-pockets/project5/cmd/converter/main.go
+++ learngo-pockets/project5/cmd/converter/main.go
@@ -1,3 +1,54 @@
 package main
 
-func main() {}
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"moneyconverter/money"
+)
+
+func main() {
+	from := flag.String("from", "", "source currency, required")
+	to := flag.String("to", "EUR", "target currency")
+	flag.Parse()
+
+	fromCurrency, err := money.ParseCurrency(*from)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to parse value %q: %s\n", *from, err.Error())
+		os.Exit(1)
+	}
+
+	toCurrency, err := money.ParseCurrency(*to)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to parse value %q: %s\n", *to, err.Error())
+		os.Exit(1)
+	}
+
+	value := flag.Arg(0)
+	if value == "" {
+		fmt.Fprintf(os.Stderr, "mising amount to convert\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	quantity, err := money.ParseDecimal(value)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to parse value %q: %s\n", value, err.Error())
+		os.Exit(1)
+	}
+
+	amount, err := money.NewAmount(quantity, fromCurrency)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		os.Exit(1)
+	}
+
+	convertedAmount, err := money.Convert(amount, toCurrency)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Fprintf(os.Stdout, "%s = %s\n", amount.String(), convertedAmount.String())
+}

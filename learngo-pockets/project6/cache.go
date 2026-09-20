09b9f73ecefa1675c3d2db0e313c7cdfbba9bc54
Mohammed ElDegwi <mohammedeldegwi@gmail.com>
2026-09-20 10:40:17 +0300

    feat: implement basic cache operations
    Signed-off-by: Mohammed ElDegwi <mohammedeldegwi@gmail.com>

---
+++ learngo-pockets/project6/cache.go
@@ -0,0 +1,20 @@
+package cache
+
+// Cache is a gerneric cache, its key can be any comparale value and the attached,
+// value can be anything.
+type Cache[K comparable, V any] struct {
+	data map[K]V
+}
+
+// New makes a new instance of the Cache struct.
+func New[K comparable, V any]() Cache[K, V] {
+	return Cache[K, V]{
+		data: make(map[K]V),
+	}
+}
+
+// Read returns the associated value of a key.
+func (c *Cache[K, V]) Read(key K) (V, bool) {
+	v, found := c.data[key]
+	return v, found
+}

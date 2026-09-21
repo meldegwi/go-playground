dab4f6c1d4d079c92d5070a1060939c9cc24c78f
Mohammed ElDegwi <mohammedeldegwi@gmail.com>
2026-09-21 12:21:53 +0300

    feat: add ttl to cache
    Signed-off-by: Mohammed ElDegwi <mohammedeldegwi@gmail.com>

--- learngo-pockets/project6/cache.go
+++ learngo-pockets/project6/cache.go
@@ -1,29 +1,49 @@
 package cache
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
 
 // Cache is a gerneric cache, its key can be any comparale value and the attached,
 // value can be anything.
 type Cache[K comparable, V any] struct {
-	mu   sync.RWMutex
-	data map[K]V
+	ttl  time.Duration
+	mu   sync.Mutex
+	data map[K]EntryWithTimeout[V]
+}
+
+type EntryWithTimeout[V any] struct {
+	value   V
+	expires time.Time // after that time the value is expired.
 }
 
 // New makes a new instance of the Cache struct.
-func New[K comparable, V any]() Cache[K, V] {
+func New[K comparable, V any](ttl time.Duration) Cache[K, V] {
 	return Cache[K, V]{
-		data: make(map[K]V),
+		ttl:  ttl,
+		data: make(map[K]EntryWithTimeout[V]),
 	}
 }
 
 // Read returns the associated value of a key.
 func (c *Cache[K, V]) Read(key K) (V, bool) {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
+	c.mu.Lock()
+	defer c.mu.Unlock()
 
-	v, found := c.data[key]
+	var zeroV V
 
-	return v, found
+	e, ok := c.data[key]
+
+	switch {
+	case !ok:
+		return zeroV, false
+	case e.expires.Before(time.Now()):
+		delete(c.data, key)
+		return zeroV, false
+	default:
+		return e.value, true
+	}
 }
 
 // Upsert overrides the value for a given key.
@@ -31,7 +51,10 @@ func (c *Cache[K, V]) Upsert(key K, value V) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	c.data[key] = value
+	c.data[key] = EntryWithTimeout[V]{
+		value:   value,
+		expires: time.Now().Add(c.ttl),
+	}
 
 	return nil
 }
--- learngo-pockets/project6/cache_external_test.go
+++ learngo-pockets/project6/cache_external_test.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"sync"
 	"testing"
+	"time"
 
 	"cache"
+
+	"github.com/alecthomas/assert/v2"
 )
 
 func TestCache_Parallel_Goroutines(t *testing.T) {
-	c := cache.New[int, string]()
+	c := cache.New[int, string](time.Millisecond * 100)
 
 	const parallelTasks = 10
 
@@ -25,7 +28,7 @@ func TestCache_Parallel_Goroutines(t *testing.T) {
 }
 
 func TestCache_Parallel(t *testing.T) {
-	c := cache.New[int, string]()
+	c := cache.New[int, string](time.Millisecond * 100)
 
 	t.Run("write six", func(t *testing.T) {
 		t.Parallel()
@@ -36,4 +39,22 @@ func TestCache_Parallel(t *testing.T) {
 		t.Parallel()
 		c.Upsert(6, "boo")
 	})
+}
+
+func TestCache_TTL(t *testing.T) {
+	t.Parallel()
+
+	c := cache.New[string, string](time.Millisecond * 100)
+
+	c.Upsert("Egyptian", "Red")
+
+	got, found := c.Read("Egyptian")
+	assert.True(t, found)
+	assert.Equal(t, "Red", got)
+
+	time.Sleep(time.Millisecond * 200)
+
+	got, found = c.Read("Egyptian")
+	assert.False(t, found)
+	assert.Equal(t, "", got)
 }
